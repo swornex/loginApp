a0e33224eb8c16aa1ccc97315fce02c2cf5056f8
Swornima <sworstha@gmail.com>
2023-05-04 10:51:54 +0545

    changes on firebase cred

--- src/components/firebaseConnect.tsx
+++ src/components/firebaseConnect.tsx
@@ -4,12 +4,12 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: "loginapp-592bf.firebaseapp.com",
-    projectId: "loginapp-592bf",
-    storageBucket: "loginapp-592bf.appspot.com",
-    messagingSenderId: "94748544133",
-    appId: "1:94748544133:web:d20293a62e502479d76dec",
-    measurementId: "G-EMNDPM35HP"
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_STORAGE_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
