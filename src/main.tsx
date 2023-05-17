d68104eb50bc788b5afa38279b92a3b59e69d9d6
Swornima <sworstha@gmail.com>
2023-05-17 10:38:30 +0545

    enhance get todo lists by using react query

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/sass/style.scss";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools />
+        </QueryClientProvider>
     </React.StrictMode>
 );
