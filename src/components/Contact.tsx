d2ad3739b0d4d2b092b5d6f253300e13ad91304e
Swornima <sworstha@gmail.com>
2023-05-30 14:18:24 +0545

    change content in about and contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,13 @@
+import construction from "../assets/images/construction.png";
+
 const Contact = () => {
-    return <h1>Welcome to Contact Page</h1>;
+    return (
+        <>
+            <h1>Whoops! The page is under construction.</h1>
+            <img src={construction} width="500px" />
+            <h2> Will be there soon.</h2>
+        </>
+    );
 };
 
 export default Contact;
